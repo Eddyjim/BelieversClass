101148ea862897763ad55a2a4a819013d50d0d27
Edward <edward.jimenez.m@gmail.com>
2012-04-11 10:43:03 -0500

    desde 0

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-ClassManager::Application.config.session_store :cookie_store, :key => '_ClassManager_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# ClassManager::Application.config.session_store :active_record_store
